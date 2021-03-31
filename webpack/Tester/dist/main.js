7625620b96b886e9768ab45b67e080f9c60105e4
jianghurong <1164310634@qq.com>
2021-03-31 17:16:35 +0800

    update

---
+++ webpack/Tester/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var e;e=function(e,n){let a=document.createElement("p");return a.innerText="I hava a pen, I have a app",a=a,a}(),document.body.appendChild(e)})();
---
+++ webpack/Tester/src/format.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: Richard Chiang
+ * @Date: 2021-03-31 10:50:29
+ * @LastEditor: Richard Chiang
+ * @LastEditTime: 2021-03-31 10:50:29
+ * @Email: 19875991227@163.com
+ * @Description: 
+ */
